c7a92f59c042c1e4c91f79a6c5e2591c51074b2b
Md Saiful Islam Rimon <50659790+saifulislamrimon2014@users.noreply.github.com>
2024-09-09 03:03:03 +0600

    Add files via upload

---
+++ src/MovieTheater.java
@@ -0,0 +1,7 @@
+public class MovieTheater extends OthersFacility{
+
+    @Override
+    void facilities() {
+        System.out.println("3D Movie 4k: 1200 BDT/ticket\nGeneral Movie: 300 BDT/ticket");
+    }
+}
---
+++ src/SwimmingPool.java
@@ -0,0 +1,6 @@
+public class SwimmingPool extends OthersFacility{
+    @Override
+     void facilities() {
+        System.out.println("Common Swimming pool cost: 300 BDT per hour!\nVIP Personal pool cost: 4000BDT per hour!");
+    }
+}
